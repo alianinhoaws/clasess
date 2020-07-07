93b7e021b0d266945f0c9cce2b55d005557e37b9
Andrii Mazurok <amaz1@rv6489.softservecom.com>
2020-07-07 23:32:30 +0300

    server inheritance HW/08/07/2020

--- serverOOP/models.py
+++ serverOOP/models.py
@@ -21,42 +21,36 @@ class RequestPatcher:
         return request[1].split('/')[1]
 
     def parse_args(self, request):
-        return request[-1].split('\n')[-1]
+        return re.findall('=(\w+)', request[-1].split('\n')[-1])
 
     def parse_id(self, request):
         return request[1].split('/')[2]
 
-    def save(self, request, args):
-        return UserProfile(self.parse_id(request), args[0], args[1], args[2], args[3])() \
-            if self.parse_url(request) == 'users' else Companies(self.parse_id(request), args[0], args[1], args[2])()
+    def save(self, args):
+        pass
 
     def get(self, request):
         return STORAGE.get(self.parse_url(request), {}).get(self.parse_id(request))
 
     def post(self, request):
         # input data
-        args = re.findall('=(\w+)', self.parse_args(request))
         if STORAGE.get(self.parse_url(request), {}).get(self.parse_id(request)):
             return
-        return STORAGE.get(self.parse_url(request), {}).update({self.parse_id(request): (self.save(request, args))})
+        return STORAGE.get(self.parse_url(request), {}).update(
+            {self.parse_id(request): (self.save(self.parse_args(request)))}
+        )
 
     def put(self, request):
         # update data
         if not STORAGE.get(self.parse_url(request), {}).get(self.parse_id(request)):
             return
-        args = re.findall('=(\w+)', self.parse_args(request))
-        return STORAGE.get(self.parse_url(request), {}).update({self.parse_id(request): (self.save(request, args))})
+        return STORAGE.get(self.parse_url(request), {}).update({self.parse_id(request): (self.save(request))})
 
     def delete(self, request):
         try:
             STORAGE.get(self.parse_url(request)).pop(self.parse_id(request))
         except KeyError:
             return
-
-    def __call__(self):
-        response = self.method_dispatcher(self.parse_method(self.request))(self.request)
-        print(response)
-        return str(response) if response else 'OK'
 
     def method_dispatcher(self, method):
         try:
@@ -71,29 +65,46 @@ class RequestPatcher:
             return f'Method {ex} not allowed'
 
 
-class UserProfile(RequestPatcher):
+class AbstractModels(RequestPatcher):
 
-    def __init__(self, id, name, surname, birthday, telephone):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.birthday = birthday
-        self.telephone = telephone
+    def __init__(self, request):
+        super().__init__(request)
 
     def __call__(self):
+        if self.parse_url(self.request) == 'users':
+            return UserProfile(self.request)()
+        return Companies(self.request)()
+
+
+class UserProfile(AbstractModels):
+
+    def __init__(self, request):
+        self.request = request
+
+    def __call__(self):
+        response = self.method_dispatcher(self.parse_method(self.request))(self.request)
+        print(STORAGE)
+        return str(response) if response else 'OK'
+
+    def save(self, args):
+        name, surname, birthday, telephone = args
         return {
-            'id': self.id, 'name': self.name, 'surname': self.surname,
-            'birthday': self.birthday, 'telephone': self.telephone
+            'id': self.parse_id(self.request), 'name': name, 'surname': surname,
+            'birthday': birthday, 'telephone': telephone
         }
 
 
-class Companies(RequestPatcher):
+class Companies(AbstractModels):
 
-    def __init__(self, id, name, address, telephone):
-        self.id = id
-        self.name = name
-        self.address = address
-        self.telephone = telephone
+    def __init__(self, request):
+        self.request = request
 
     def __call__(self):
-        return {'id': self.id, 'name': self.name, 'address': self.address, 'telephone': self.telephone}
+        response = self.method_dispatcher(self.parse_method(self.request))(self.request)
+        print(STORAGE)
+        return str(response) if response else 'OK'
+
+    def save(self, args):
+        name, surname, address, telephone = args
+        return {'id': self.parse_id(self.request), 'name': name,
+                'address': address, 'telephone': telephone}
--- serverOOP/server.py
+++ serverOOP/server.py
@@ -1,6 +1,6 @@
 import socket
 from threadpool import ThreadPool
-from models import RequestPatcher
+from models import AbstractModels
 
 
 class Server:
@@ -23,16 +23,16 @@ class Server:
     def job(self, client_socket):
         self.request = client_socket.recv(1024)
         if self.request:
-            response = RequestPatcher(request=self.request)()
+            response = str(AbstractModels(request=self.request)())
+            print("RESPONSE", response)
             client_socket.sendall(response.encode())
             client_socket.close()
 
 
 if __name__ == '__main__':
-    a = Server('localhost', 8080)
-    a.run()
+    server = Server('localhost', 8080)
+    server.run()
 
-#curl -X GET -d arg=val -d arg2=val2 localhost:8080
 #curl -X POST -d id=12 -d name='companyX' -d address='London' -d telephone='001' localhost:8080/companies/12
 #curl -X GET localhost:8080/companies/12
 #curl -X POST -d id=1 -d name='John' -d surname='D' -d birthday='07/07/2010' -d telephone='+380671111333' localhost:8080/users/1
