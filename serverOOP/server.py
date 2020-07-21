7e1d7346dcc73ff3a9532319a214cde0835b70ee
alianinhoaws <49166503+alianinhoaws@users.noreply.github.com>
2020-07-22 01:47:29 +0300

    Update server.py
    Added examples

--- serverOOP/server.py
+++ serverOOP/server.py
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     server = Server('localhost', 8080)
     server.run()
 
-#curl -X POST -d name='companyX' -d address='London' -d telephone='001' localhost:8080/companies/
-#curl -X PUT-d name='companyX' -d address='London' -d telephone='001' localhost:8080/companies/1
+#curl -X POST -d name='CompanyX' -d address='london' -d telephone='0685930245' localhost:8080/companies/
+#curl -X PUT-d name='CompanyX' -d address='london' -d telephone='0685930245' localhost:8080/companies/1
 #curl -X GET localhost:8080/companies/12
-#curl -X POST -d id=1 -d name='John' -d surname='D' -d birthday='07/07/2010' -d telephone='+380671111333' localhost:8080/users/1
+#curl -X POST -d id=1 -d name='John' -d surname='Dan' -d birthday='07/07/2010' -d telephone='0685930245' localhost:8080/users/1
 #curl -X GET localhost:8080/users/1
